bf0369c7d7720309a8bf170fbbe31d9f86d3bcc1
SeungHak5161 <bsh93367@gmail.com>
2023-03-04 23:53:23 +0900

    🎨 make main layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
 import "styles/layout.scss";
 export const Layout = () => {
-  return <section className="layout"></section>;
+  const [menuOpened, setMenuOpened] = useState(false);
+  return (
+    <main id="layout">
+      <aside
+        id="menu"
+        className={menuOpened ? "menuOpend" : "menuClosed"}
+      ></aside>
+      {menuOpened ? (
+        <button className="menuBtn" onClick={() => setMenuOpened(!menuOpened)}>
+          <div id="closeMenuT"></div>
+          <div id="closeMenuM"></div>
+          <div id="closeMenuB"></div>
+        </button>
+      ) : (
+        <button className="menuBtn" onClick={() => setMenuOpened(!menuOpened)}>
+          <div id="openMenuT"></div>
+          <div id="openMenuM"></div>
+          <div id="openMenuB"></div>
+        </button>
+      )}
+      <button id="addBtn">
+        <div id="plusIcon">
+          <div id="plusIconH"></div>
+          <div id="plusIconV"></div>
+        </div>
+      </button>
+    </main>
+  );
 };
 export default Layout;
