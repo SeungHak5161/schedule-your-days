990b115372ba24f39d8201e6936edf6697a9b247
SeungHak5161 <bsh93367@gmail.com>
2023-03-09 01:32:56 +0900

    🚧 create addItem layer
    - db에 추가 안붙임,
    - colorPicker 기능 필요

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,46 @@
+export const AddItem = ({ bottomOpened, setBottomOpened }) => {
+  return (
+    <div
+      className={`bottomUpLayer ${
+        bottomOpened ? "bottomOpend" : "bottomClosed"
+      }`}
+    >
+      <div id="xIcon" onClick={() => setBottomOpened(false)}>
+        <div id="xIconFirst"></div>
+        <div id="xIconSecond"></div>
+      </div>
+      <div id="addItemContentWrapper">
+        <div className="addItemElem">
+          <div className="addItemElemInput">
+            <input
+              id="itemName"
+              className="inputLarge"
+              placeholder="제목"
+            ></input>
+          </div>
+          <div id="colorPicker"></div>
+        </div>
+        <div className="addItemElem">
+          <div className="addItemElemInput">
+            <input
+              id="itemContent"
+              className="inputMedium"
+              placeholder="내용"
+            ></input>
+          </div>
+        </div>
+        <div className="addItemElem">
+          <div className="addItemElemInput">
+            <input
+              id="itemTime"
+              className="inputMedium"
+              placeholder="시간"
+            ></input>
+          </div>
+        </div>
+        <button id="addItemBtn">추 가</button>
+      </div>
+    </div>
+  );
+};
+export default AddItem;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import "styles/layout.scss";
 import AddBtn from "./AddBtn";
+import AddItem from "./AddItem";
 import Calendar from "./Calendar";
 import Navbar from "./Navbar";
+import ShieldLayer from "./ShieldLayer";
 import SideMenu from "./SideMenu";
 export const Layout = () => {
   const [menuOpened, setMenuOpened] = useState(false);
+  const [bottomOpened, setBottomOpened] = useState(false);
   return (
     <section id="layout">
       <SideMenu menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
-      <main className={menuOpened ? "opacity05" : null}>
+      <AddItem bottomOpened={bottomOpened} setBottomOpened={setBottomOpened} />
+      <main
+        className={`${menuOpened && "op05-ts02"} ${
+          bottomOpened && "op05-ts03"
+        }`}
+      >
         <Navbar menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
         <Calendar />
+        <AddBtn setBottomOpened={setBottomOpened} />
+        <ShieldLayer
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+          bottomOpened={bottomOpened}
+          setBottomOpened={setBottomOpened}
+        />
       </main>
-      <AddBtn />
     </section>
   );
 };
